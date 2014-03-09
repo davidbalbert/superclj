32bf4cf9a471180552d7e56aeae2e774d3d5533d
David Albert <davidbalbert@gmail.com>
2014-03-09 15:41:09 -0400

    implemented clc, sec, and xce

--- src/superclj/65816.clj
+++ src/superclj/65816.clj
@@ -1,7 +1,9 @@
-(ns superclj.65816)
+(ns superclj.65816
+  (:require [superclj.memory :as mem]
+            [superclj.asm :as asm]))
 
 (defn new-cpu []
-  {:emulation-mode 1
+  {:emulation-mode 0x1
    :registers {
                :a 0x00
                :x 0x00
@@ -11,4 +13,56 @@
                :pc 0
                :program-bank 0x00
                :data-bank 0x00
-               :status 0}})
+               :status 2r00110000}})
+
+(defn- pc [cpu]
+  ((cpu :registers) :pc))
+
+(defn- status [cpu]
+  ((cpu :registers) :status))
+
+(defn- carry [cpu]
+  (bit-and (status cpu) 0x1))
+
+(defn- increment-pc [cpu]
+  (let [new-pc (inc (pc cpu))]
+    (assoc-in cpu [:registers :pc] new-pc)))
+
+(defn- assign-bit [byte position value]
+  (if (zero? value)
+    (bit-clear byte position)
+    (bit-set byte position)))
+
+(defn step [cpu mem]
+  (let [opcode (mem/load-byte mem (pc cpu))]
+    ((opcodes opcode) (increment-pc cpu) mem)))
+
+(defn run [cpu mem]
+  (if (< (pc cpu) (count mem))
+    (let [[next-cpu next-mem] (step cpu mem)]
+      (recur next-cpu next-mem))
+    [cpu mem]))
+
+(defn clc [cpu mem]
+  (let* [new-status (bit-and (status cpu) 2r11111110)
+         new-cpu (assoc-in cpu [:registers :status] new-status)]
+    [new-cpu mem]))
+
+(defn sec [cpu mem]
+  (let* [new-status (bit-or (status cpu) 0x1)
+         new-cpu (assoc-in cpu [:registers :status] new-status)]
+    [new-cpu mem]))
+
+(defn xce [cpu mem]
+  (let [new-status (assign-bit (status cpu) 0 (cpu :emulation-mode))
+        new-emulation-mode (carry cpu)]
+    [(assoc-in (assoc cpu :emulation-mode new-emulation-mode)
+               [:registers :status]
+               new-status)
+     mem]))
+
+(def opcodes {
+              0x18 clc
+              0x38 sec
+              0xFB xce
+              })
--- src/superclj/asm.clj
+++ src/superclj/asm.clj
@@ -1,2 +1,15 @@
 (ns superclj.asm)
 
+
+(defn- assemble-one [ins]
+  (cond
+   (= ins '(clc)) [0x18]
+   (= ins '(sec)) [0x38]
+   (= ins '(xce)) [0xFB]
+   :else (throw (IllegalArgumentException.
+                 (str ins " is not a valid instruction sequence")))))
+
+(assemble-one '(clc))
+
+(defn asm [instructions]
+  (vec (mapcat assemble-one instructions)))
