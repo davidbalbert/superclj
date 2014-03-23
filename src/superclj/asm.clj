d9bde5fa5956bb566f36e61771e98406abc3b239
David Albert <davidbalbert@gmail.com>
2014-03-23 19:47:35 -0400

    asm: DS and partial DC (int and address)

--- src/superclj/asm.clj
+++ src/superclj/asm.clj
@@ -1,6 +1,14 @@
 (ns superclj.asm)
 
-(defn- assemble-one [ins labels]
+(defn- double-to-byte [value]
+  (let [high (bit-shift-right (bit-and 0xFF00 value) 8)
+        low (bit-and 0xFF value)]
+    [high low]))
+
+(defn- flip-bytes [[high low]]
+  [low high])
+
+(defn- assemble-one [[op & args :as ins] labels]
   (cond
    (= ins '(clc)) [0x18]
    (= ins '(sec)) [0x38]
@@ -10,6 +18,14 @@
    (= ins '(tay)) [0xA8]
    (= ins '(txa)) [0x8A]
    (= ins '(tya)) [0x98]
+   (= op 'ds) (let [bytes (first args)]
+                (vec (repeat bytes 0)))
+   (= op 'dc) (let [[const-type value] args]
+                (cond
+                 (= 'i1 const-type) [(bit-and 0xFF value)]
+                 (= 'a const-type)  (flip-bytes (double-to-byte value))
+                 :else (throw (IllegalArgumentException.
+                               (str const-type " is not a valid constant type")))))
    :else (throw (IllegalArgumentException.
                  (str ins " is not a valid instruction sequence")))))
 
