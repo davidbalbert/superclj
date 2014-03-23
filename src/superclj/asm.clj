c94a383a4eafb12c7b3bf9875f7b93a980c309dc
David Albert <davidbalbert@gmail.com>
2014-03-23 19:21:01 -0400

    Add support for labels in the assembler
    Don't do anything with them though.

--- src/superclj/asm.clj
+++ src/superclj/asm.clj
@@ -1,6 +1,6 @@
 (ns superclj.asm)
 
-(defn- assemble-one [ins]
+(defn- assemble-one [ins labels]
   (cond
    (= ins '(clc)) [0x18]
    (= ins '(sec)) [0x38]
@@ -13,5 +13,21 @@
    :else (throw (IllegalArgumentException.
                  (str ins " is not a valid instruction sequence")))))
 
+(defn- read-labels [instructions]
+  (let [numbered-instructions (map-indexed (fn [i ins] [i ins]) instructions)
+        labels (reduce (fn [labels [line-number ins]]
+                         (if (= (first ins) 'label)
+                           (assoc labels (second ins) line-number)
+                           labels))
+                       {} numbered-instructions)
+        delabeled-instructions (map (fn [[op & args :as ins]]
+                                      (if (= op 'label)
+                                        (second args)
+                                        ins))
+                                    instructions)]
+    [labels delabeled-instructions]))
+
 (defn asm [instructions]
-  (vec (mapcat assemble-one instructions)))
+  (let [[labels delabeled-instructions] (read-labels instructions)]
+    (vec (mapcat (fn [ins] (assemble-one ins labels))
+                 delabeled-instructions))))
