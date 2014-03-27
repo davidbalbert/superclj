b49c2c42e525f2bb28dc7c1918d1dfd6ad0e7ae1
David Albert <davidbalbert@gmail.com>
2014-03-26 22:02:40 -0400

    Clean up tests

--- test/superclj/65816_test.clj
+++ test/superclj/65816_test.clj
@@ -1,8 +1,6 @@
 (ns superclj.65816-test
   (:require [clojure.test :refer :all]
             [superclj.65816 :refer :all]
-
-(def c (new-cpu))
             [superclj.asm :as asm]
             [superclj.memory :as mem]))
 
@@ -12,48 +10,43 @@
 (defn with-carry-set [cpu]
   (assoc cpu :status (bit-set (status cpu) 0)))
 
-(defn with-carry-cleared [cpu]
-  (assoc cpu :status (bit-clear (status cpu) 0)))
-
 (deftest clc-should-clear-carry
-  (let [start-cpu (with-carry-set c)
+  (let [start-cpu (with-carry-set (new-cpu))
         end-cpu (first (run-asm start-cpu '((clc))))]
     (is (= 0 (carry end-cpu)))))
 
 (deftest sec-should-set-carry
-  (let [start-cpu (with-carry-cleared c)
-        end-cpu (first (run-asm start-cpu '((sec))))]
-    (is (= 1 (carry end-cpu)))))
+  (let [cpu (first (run-asm (new-cpu) '((sec))))]
+    (is (= 1 (carry cpu)))))
 
 (deftest xce-should-exchange-carry-and-emulation-bits
-  (let [start-cpu (with-carry-cleared c)
-        end-cpu (first (run-asm start-cpu '((xce))))]
-    (is (= 0 (:emulation-mode end-cpu)))
-    (is (= 1 (carry end-cpu)))))
+  (let [cpu (first (run-asm (new-cpu) '((xce))))]
+    (is (= 0 (cpu :emulation-mode)))
+    (is (= 1 (carry cpu)))))
 
 (deftest txy-should-transfer-x-to-y
-  (let [start-cpu (assoc c :x 0x10)
+  (let [start-cpu (assoc (new-cpu) :x 0x10)
         end-cpu (first (run-asm start-cpu '((txy))))]
-    (is (= 0x10 (:y end-cpu)))))
+    (is (= 0x10 (end-cpu :y)))))
 
 (deftest tax-should-transfer-a-to-x
   (let [start-cpu (assoc (new-cpu) :a 0x10)
         end-cpu (first (run-asm start-cpu '((tax))))]
-    (is (= 0x10 (:x end-cpu)))))
+    (is (= 0x10 (end-cpu :x)))))
 
 (deftest tay-should-transfer-a-to-y
   (let [start-cpu (assoc (new-cpu) :a 0x10)
         end-cpu (first (run-asm start-cpu '((tay))))]
-    (is (= 0x10 (:y end-cpu)))))
+    (is (= 0x10 (end-cpu :y)))))
 
 (deftest txa-should-transfer-x-to-a
   (let [start-cpu (assoc (new-cpu) :x 0x10)
         end-cpu (first (run-asm start-cpu '((txa))))]
-    (is (= 0x10 (:a end-cpu)))))
+    (is (= 0x10 (end-cpu :a)))))
 
 (deftest tya-should-transfer-y-to-a
   (let [start-cpu (assoc (new-cpu) :y 0x10)
         end-cpu (first (run-asm start-cpu '((tya))))]
-    (is (= 0x10 (:a end-cpu)))))
+    (is (= 0x10 (end-cpu :a)))))
 
 (run-tests)
