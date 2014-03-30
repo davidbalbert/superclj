a8ce3c843a0191d302ca84b6435cf4cc5a3dc473
David Albert <davidbalbert@gmail.com>
2014-03-30 18:18:26 -0400

    asm: new label syntax and actual label support

--- src/superclj/asm.clj
+++ src/superclj/asm.clj
@@ -1,12 +1,14 @@
 (ns superclj.asm)
 
-(defn- double-to-byte [value]
+(defn- double->bytes [value]
   (let [high (bit-shift-right (bit-and 0xFF00 value) 8)
         low (bit-and 0xFF value)]
     [high low]))
 
 (defn- swap-bytes [[high low]]
   [low high])
+
+(def double->address (comp swap-bytes double->bytes))
 
 (defn- num->byte-vector [number]
   (loop [n number result []]
@@ -15,7 +17,7 @@
       (recur (bit-shift-right n 8)
              (conj result (bit-and 0xFF n))))))
 
-(defn- assemble-one [[op & args :as ins] labels]
+(defn- generate-intermediate-code [[op & args :as ins]]
   (cond
    (= ins '(clc)) [0x18]
    (= ins '(sec)) [0x38]
@@ -26,12 +28,13 @@
    (= ins '(txa)) [0x8A]
    (= ins '(tya)) [0x98]
    (= ins '(stp)) [0xDB]
+   (= op 'lda)    [0xAD (first args)]
    (= op 'ds) (let [bytes (first args)]
                 (vec (repeat bytes 0)))
    (= op 'dc) (let [[const-type value] args]
                 (cond
                  (= 'i1 const-type) [(bit-and 0xFF value)]
-                 (= 'a const-type)  (swap-bytes (double-to-byte value))
+                 (= 'a const-type)  (double->address value)
                  (= 'c const-type)  (map byte (vec value))
                  (= 'h const-type)  (num->byte-vector value)
                  :else (throw (IllegalArgumentException.
@@ -39,24 +42,87 @@
    :else (throw (IllegalArgumentException.
                  (str ins " is not a valid instruction sequence")))))
 
-(defn read-labels [instructions]
-  (reduce (fn [labels [i [op name]]]
-            (if (= 'label op)
-              (assoc labels name i)
+(defn mapify-instruction [ins]
+  {:instruction ins})
+
+(defn label-first [label instructions]
+  (let [labeled-first-instruction (assoc (first instructions) :label label)]
+    (cons labeled-first-instruction (rest instructions))))
+
+(defn inline-label [[label & instructions]]
+   (let [instructions-as-maps (map mapify-instruction instructions)]
+     (if (= label '_)
+       instructions-as-maps
+       (label-first label instructions-as-maps))))
+
+;; Labels represent addresses, which are assumed to be two bytes.
+;; This may be different in long addressing modes, but I'm not sure.
+(defn count-intermediate-code [v]
+  (reduce + (map (fn [byte-or-symbol]
+                   (if (symbol? byte-or-symbol)
+                     2
+                     1))
+                 v)))
+
+(defn assemble-one [instruction]
+  (let [intermediate-code (generate-intermediate-code (instruction :instruction))
+        size (count-intermediate-code intermediate-code)]
+    (-> instruction
+        (assoc :intermediate-code intermediate-code)
+        (assoc :size size))))
+
+(defn running-totals [nums]
+  (reduce (fn [totals n]
+            (conj totals (+ n (or (last totals) 0))))
+          []
+          nums))
+
+(defn calculate-offsets [instructions]
+  (let [sizes (map :size instructions)
+        totals (running-totals sizes)
+        offsets (concat [0] (butlast totals))]
+    (map (fn [ins offset]
+           (assoc ins :offset offset))
+         instructions
+         offsets)))
+
+(defn make-labels [instructions]
+  (reduce (fn [labels ins]
+            (if (contains? ins :label)
+              (if (contains? labels (ins :label))
+                (throw
+                 (IllegalArgumentException.
+                  (str "Label `"(ins :label) "' has already been used")))
+                (assoc labels (ins :label) (double->address (ins :offset))))
               labels))
           {}
-          (map-indexed vector instructions)))
+          instructions))
 
-(defn remove-labels [instructions]
-  (vec (map
-        (fn [[op :as ins]]
-          (if (= 'label op)
-            (nth ins 2)
-            ins))
-        instructions)))
+(defn replace-label [intermediate-code labels]
+  (flatten (map (fn [byte-or-symbol]
+                  (if (symbol? byte-or-symbol)
+                    (if (contains? labels byte-or-symbol)
+                      (labels byte-or-symbol)
+                      (throw
+                       (IllegalArgumentException.
+                        (str "Unknown label `" byte-or-symbol "'"))))
+                    byte-or-symbol))
+                intermediate-code)))
 
-(defn asm [instructions]
-  (let [labels (read-labels instructions)
-        delabeled-instructions (remove-labels instructions)]
-    (vec (mapcat (fn [ins] (assemble-one ins labels))
-                 delabeled-instructions))))
+(defn replace-labels [instructions labels]
+  (let [machine-code (map (fn [ins]
+                            (replace-label (ins :intermediate-code) labels))
+                          instructions)]
+    (map (fn [ins bytes]
+           (assoc ins :machine-code bytes))
+         instructions
+         machine-code)))
+
+;; asm syntax: (& [label & instructions])
+(defn asm [labeled-fragments]
+  (let [labeled-instructions (mapcat inline-label labeled-fragments)
+        intermediate-instructions (map assemble-one labeled-instructions)
+        offset-instructions (calculate-offsets intermediate-instructions)
+        labels (make-labels offset-instructions)
+        assembeled-instructions (replace-labels offset-instructions labels)]
+    (vec (mapcat :machine-code assembeled-instructions))))
--- test/superclj/65816_test.clj
+++ test/superclj/65816_test.clj
@@ -12,46 +12,55 @@
 
 (deftest clc-should-clear-carry
   (let [start-cpu (with-carry-set (new-cpu))
-        end-cpu (first (run-asm start-cpu '((clc) (stp))))]
+        end-cpu (first (run-asm start-cpu '((_ (clc)
+                                               (stp)))))]
     (is (= 0 (carry end-cpu)))))
 
 (deftest sec-should-set-carry
-  (let [cpu (first (run-asm (new-cpu) '((sec) (stp))))]
+  (let [cpu (first (run-asm (new-cpu) '((_ (sec)
+                                           (stp)))))]
     (is (= 1 (carry cpu)))))
 
 (deftest xce-should-exchange-carry-and-emulation-bits
-  (let [cpu (first (run-asm (new-cpu) '((xce) (stp))))]
+  (let [cpu (first (run-asm (new-cpu) '((_ (xce)
+                                           (stp)))))]
     (is (= 0 (cpu :emulation-mode)))
     (is (= 1 (carry cpu)))))
 
 (deftest txy-should-transfer-x-to-y
   (let [start-cpu (assoc (new-cpu) :x 0x10)
-        end-cpu (first (run-asm start-cpu '((txy) (stp))))]
+        end-cpu (first (run-asm start-cpu '((_ (txy)
+                                               (stp)))))]
     (is (= 0x10 (end-cpu :y)))))
 
 (deftest tax-should-transfer-a-to-x
   (let [start-cpu (assoc (new-cpu) :a 0x10)
-        end-cpu (first (run-asm start-cpu '((tax) (stp))))]
+        end-cpu (first (run-asm start-cpu '((_ (tax)
+                                               (stp)))))]
     (is (= 0x10 (end-cpu :x)))))
 
 (deftest tay-should-transfer-a-to-y
   (let [start-cpu (assoc (new-cpu) :a 0x10)
-        end-cpu (first (run-asm start-cpu '((tay) (stp))))]
+        end-cpu (first (run-asm start-cpu '((_ (tay)
+                                               (stp)))))]
     (is (= 0x10 (end-cpu :y)))))
 
 (deftest txa-should-transfer-x-to-a
   (let [start-cpu (assoc (new-cpu) :x 0x10)
-        end-cpu (first (run-asm start-cpu '((txa) (stp))))]
+        end-cpu (first (run-asm start-cpu '((_ (txa)
+                                               (stp)))))]
     (is (= 0x10 (end-cpu :a)))))
 
 (deftest tya-should-transfer-y-to-a
   (let [start-cpu (assoc (new-cpu) :y 0x10)
-        end-cpu (first (run-asm start-cpu '((tya) (stp))))]
+        end-cpu (first (run-asm start-cpu '((_ (tya)
+                                               (stp)))))]
     (is (= 0x10 (end-cpu :a)))))
 
 (deftest stp-should-stop-the-processor
   (let [start-cpu (new-cpu)
-        end-cpu (first (run-asm start-cpu '((stp) (sec))))]
+        end-cpu (first (run-asm start-cpu '((_ (stp)
+                                               (sec)))))]
     (is (= 0 (carry end-cpu)))))
 
 (run-tests)
